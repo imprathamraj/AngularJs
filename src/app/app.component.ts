af56b4bd85746a89305e8457621d6ecd65d69d76
imprathamraj <rjpratham05@gmail.com>
2025-03-06 11:37:56 +0530

    [Pratham Raj] Add. To display Hello from BridgeLabz with a user inputted name with validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,FormsModule],
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
@@ -13,8 +14,23 @@ export class AppComponent {
   imgUrl = "../assets/images/BL_logo_square_jpg.jpg";
   url = "https://www.bridgelabz.com/experienced-engineers";
   userName: string = "";
-  onClick(){
+  nameError: string = "";
+
+  onClick() {
     console.log("Save button is clicked");
-    window.open(this.url,"_blank");
+    window.open(this.url, "_blank");
+  }
+
+  onInput($event: Event) {
+    console.log("Change Event Occurred!");
+    const inputElement = $event.target as HTMLInputElement;
+    this.userName = inputElement.value;
+
+    const nameRegex = /^[A-Z]{1}[a-zA-Z\s]{2,}$/;
+    if (nameRegex.test(this.userName)) {
+      this.nameError = "Name is Correct!";
+    } else {
+      this.nameError = "Name is Incorrect!";
     }
+  }
 }
