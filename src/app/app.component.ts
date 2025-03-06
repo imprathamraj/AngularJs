24efb2b515abdd494d008ff43157b296a71e9f88
imprathamraj <rjpratham05@gmail.com>
2025-03-06 10:40:23 +0530

    [Pratham Raj] Add. To display Hello from BridgeLabz with a user inputted name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet,FormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Hello from BridgeLabz.';
   imgUrl = "../assets/images/BL_logo_square_jpg.jpg";
+  url = "https://www.bridgelabz.com/experienced-engineers";
+  userName: string = "";
+  onClick(){
+    console.log("Save button is clicked");
+    window.open(this.url,"_blank");
+    }
 }
